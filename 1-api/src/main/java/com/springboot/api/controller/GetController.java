ed1257ce9a055e07e7a8e9f245c04f305ad81266
HS-hyeokjin <youngurwls@gmail.com>
2023-08-17 10:52:13 +0900

    Feat : 1-api GET 메서드

---
+++ 1-api/src/main/java/com/springboot/api/controller/GetController.java
@@ -0,0 +1,62 @@
+package com.springboot.api.controller;
+
+import com.springboot.api.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+
+    //api/v1/get-api
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String getHello() {
+        return "Hello world";
+    }
+
+    //api/v1/get-api/name
+    @GetMapping(value = "/name")
+    public String getName() {
+        return "Flature";
+    }
+
+    // url : api/v1/get-api/variable1/{String 값}
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable) {
+        return variable;
+    }
+
+    // url : api/v1/get-api/variable2/{String 값}
+    @GetMapping(value = "/variable2/{variable}")
+    public String getVariable2(@PathVariable("variable") String var) {
+        return var;
+    }
+
+    // url : api/v1/get-api/request1?name=value1&email=value2&organization=value3
+    @GetMapping(value = "/request1")
+    public String getRequestParam1(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization) {
+        return name + " " + email + " " + organization;
+    }
+
+    // url : api/v1/get-api/request2?key1=value1&key2=value2
+    @GetMapping(value = "/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param) {
+        StringBuilder sb = new StringBuilder();
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    //  url : api/v1/get-api/request3?name=value1&email=value2&organization=value3
+    @GetMapping(value = "request3")
+    public String getRequestParam3(MemberDto memberDto) {
+        return memberDto.toString();
+    }
+}
+
